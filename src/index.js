2dc7540c48e465795912c03c12711059759ef858
José Luiz Coe <joseluizcoe@gmail.com>
2019-10-26 11:59:15 -0300

    Adicionando configuracao inicial do express e dotenv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// Boa sorte! <3
+require('dotenv').config();
+const express = require(`express`);
+const app = express();
+const {PORT} = process.env;
+
+app.get('/', (request, response) => {
+  response.send('Página inicial');
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(`Aplicação rodando na porta ${PORT}`);
+});
