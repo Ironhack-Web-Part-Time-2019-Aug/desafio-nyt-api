d4faf711d76ffc8e815d41d104502db5d02dc869
José Luiz Coe <joseluizcoe@gmail.com>
2019-10-26 14:07:29 -0300

    Adicionando modulo hbs

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ app.get('/', (request, response) => {
   response.send('Página inicial');
 });
 
+app.get('/search-result', (request, response) => {
+  response.send('Resultado de busca');
+});
+
+app.get('/trendings', (request, response) => {
+  response.send('Notícias mais visualizadas');
+});
+
+app.get('/trending-by-email', (request, response) => {
+  response.send('Notícias mais compartilhadas');
+});
+
 app.listen(PORT, (error) => {
   if (error) {
     console.log(error);
