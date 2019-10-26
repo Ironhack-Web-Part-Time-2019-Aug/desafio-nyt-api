9285c5c06c1647776c4ad300b7e4ceadb852148b
Victor Nevola <victornevolapn@gmail.com>
2019-10-26 15:35:01 -0300

    Criado search

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-const {home} = require('./routes/index');
+const {home, searchResults} = require('./routes/index');
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname+'/views');
@@ -10,5 +10,6 @@ app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', home);
+app.get('/search-results', searchResults);
 
 app.listen(3001, ()=> console.log('Start server'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,24 @@
 const fetch = require('node-fetch');
+
 const home = (request, response) => {
   fetch(`https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${process.env.NYT_ID}`)
       .then((res) => res.json())
       .then((json) => {
         const result = json.results;
         response.render('home', {result});
-        console.log(result);
+      });
+};
+
+const searchResults = (request, response) => {
+  fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${request.query.q}&api-key=${process.env.NYT_ID}`)
+      .then((res) => res.json())
+      .then((json) => {
+        const result = json.response.docs;
+        response.render('searchResults', {result});
       });
 };
 
 module.exports = {
   home,
+  searchResults,
 };
