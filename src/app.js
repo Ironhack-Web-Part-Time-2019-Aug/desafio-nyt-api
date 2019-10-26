bdeb45d00d309b920e13e480453899a9b0afa4ce
Gabriel Zsigmond <gaazsig@gmail.com>
2019-10-26 12:28:47 -0300

    getting nyt api to index

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const fetch = require('node-fetch');
 const hbs = require('hbs');
 const port = 3000;
 const app = express();
@@ -11,7 +12,16 @@ app.set('views', `${__dirname}/views`);
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  fetch(
+      `https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${process.env.NYT_CLIENT_KEY}`,
+  )
+      .then((res) => res.json())
+      .then((body) => {
+        const firstTen = body.results.slice(0, 10);
+        console.log(firstTen[0]);
+        res.render('index', firstTen);
+      });
+  // res.render('index');
 });
 
 app.listen(`${port}`, () => {
