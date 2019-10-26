ba8e10ad8be26079bd96dfa4bffa216d85bdcc0b
Victor Nevola <victornevolapn@gmail.com>
2019-10-26 14:34:16 -0300

    Criado chamada de API para home

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-// const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,12 @@
+const fetch = require('node-fetch');
 const home = (request, response) => {
-  response.render('home');
+  fetch(`https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${process.env.NYT_ID}`)
+      .then((res) => res.json())
+      .then((json) => {
+        const result = json.results;
+        response.render('home', {result});
+        console.log(result);
+      });
 };
 
 module.exports = {
