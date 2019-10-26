1a6b5b954adcf026f647ceb52cee1e4c35d632fe
José Luiz Coe <joseluizcoe@gmail.com>
2019-10-26 14:08:03 -0300

    Configurando hbs e fazendo primeiro fetch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 require('dotenv').config();
 const express = require(`express`);
 const app = express();
-const {PORT} = process.env;
+const fetch = require(`node-fetch`);
+const hbs = require(`hbs`);
+const viewsPath = __dirname + '/views';
+
+const {
+  PORT, API_KEY, API_URL,
+  ARTICLE_SEARCH_PATH,
+} = process.env;
+
+app.engine('hbs', require('hbs').__express);
+app.set('view engine', 'hbs');
+app.set(`views`, viewsPath);
+hbs.registerPartials(`${viewsPath}/partials`);
 
 app.get('/', (request, response) => {
-  response.send('Página inicial');
+  response.render('index');
 });
 
 app.get('/search-result', (request, response) => {
-  response.send('Resultado de busca');
+  const {q} = request.query.q;
+  const url = `${API_URL}${ARTICLE_SEARCH_PATH}?q=${q}&api-key=${API_KEY}`;
+  fetch(url)
+      .then( (result) => result.json() )
+      .then( (result) => response.send(result.response.docs));
 });
 
 app.get('/trendings', (request, response) => {
@@ -17,6 +33,10 @@ app.get('/trendings', (request, response) => {
 
 app.get('/trending-by-email', (request, response) => {
   response.send('Notícias mais compartilhadas');
+});
+
+app.get('/about', (request, response) => {
+  response.send('Sobre');
 });
 
 app.listen(PORT, (error) => {
