317fb3aa5d1e58dafe86502f68c344fcd8c9d0a8
Victor Nevola <victornevolapn@gmail.com>
2019-10-26 11:58:13 -0300

    Criado rota para pagina principal e adicionado alguns estilo e imagem principal

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+const {home} = require('./routes/index');
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname+'/views');
 app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/partials');
 
+app.get('/', home);
 
-app.listen(3000, ()=> console.log('Start server'));
+app.listen(3001, ()=> console.log('Start server'));
