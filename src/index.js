8c24bf7facec8fb933b5cc36704d520dae3d5b66
Jeferson Silva <jeferson.s.silva1@gmail.com>
2019-10-26 17:51:51 -0300

    criando página de pesquisa

--- src/index.js
+++ src/index.js
@@ -8,23 +8,30 @@ const fetch = require('node-fetch');
 
 const app = express();
 
+const key = process.env.API;
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res, next) => {
-  fetch('https://api.nytimes.com/svc/topstories/v2/science.json?api-key=r8c8lKUCmOXNwxMoP0y5SxaUcPSexc3C')
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-  res.render('index', res);
-});
-
-app.get('/home', (req, res, next) => {
-  res.render('index');
+app.get(['/', '/home'], (req, res, next) => {
+  const news = fetch('https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=' + key)
+      .then((res) => res.json()
+          .then((json) => json));
+  news.then( (newsHome) => {
+    console.log(newsHome);
+    res.render('index', {news: newsHome.results});
+  });
 });
 
 app.get('/search-results', (req, res, next) => {
-  res.render('search-results');
+  const searchResults = fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?q={query}&api-key=' + key)
+      .then((res) => res.json()
+          .then((json) => json));
+  searchResults.then( (newsSearch) => {
+    console.log(newsSearch);
+    res.render('search-results', {searchResults: newsSearch.response.docs});
+  });
 });
 
 app.listen(3000);
