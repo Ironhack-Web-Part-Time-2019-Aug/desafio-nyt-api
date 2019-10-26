a0ceb9b620c3af1a03c2adf0f58dd6047a797398
Jeferson Silva <jeferson.s.silva1@gmail.com>
2019-10-26 13:35:54 -0300

    criando rotas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 // Boa sorte! <3
+
+require('dotenv').config();
+
+const express = require('express');
+// const hbs = 
+
+require('hbs');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.listen(3000);
