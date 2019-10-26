2497ed791f68509a36caf7ab14858dd53a91c445
Gabriel Zsigmond <gaazsig@gmail.com>
2019-10-26 13:32:41 -0300

    getting nyt api to search result

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 const hbs = require('hbs');
 const port = 3000;
+const nytKey = process.env.NYT_CLIENT_KEY;
 const app = express();
 
 hbs.registerPartials(`${__dirname}/views/components`);
@@ -13,7 +14,7 @@ app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res, next) => {
   fetch(
-      `https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${process.env.NYT_CLIENT_KEY}`,
+      `https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${nytKey}`,
   )
       .then((res) => res.json())
       .then((body) => {
@@ -21,7 +22,18 @@ app.get('/', (req, res, next) => {
         console.log(firstTen[0]);
         res.render('index', firstTen);
       });
-  // res.render('index');
+});
+
+app.get('/search-results', (req, res, next) => {
+  const searchWord = req.query.q;
+  fetch(
+      `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchWord}&api-key=${nytKey}`,
+  )
+      .then((res) => res.json())
+      .then((body) => {
+        res.render('searchResult', body.response.docs);
+        console.log(body.response.docs[0].multimedia[0].url);
+      });
 });
 
 app.listen(`${port}`, () => {
