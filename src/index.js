4f84414566dc67c13bdd23a86f0e35206dffc33a
José Luiz Coe <joseluizcoe@gmail.com>
2019-10-29 18:39:01 -0300

    Separando configurações em arquivo separado

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 require('dotenv').config();
-const express = require(`express`);
-const app = express();
-const hbs = require(`hbs`);
-const viewsPath = __dirname + '/views';
 const {PORT} = process.env;
-const {router} = require(`./routes`);
-
-app.use(express.static(`${__dirname}/public`));
-app.engine('hbs', require('hbs').__express);
-app.set('view engine', 'hbs');
-app.set(`views`, viewsPath);
-hbs.registerPartials(`${viewsPath}/partials`);
-
-app.use(router);
+const app = require(`./appConfig.js`);
 
 app.listen(PORT, (error) => {
   if (error) {
